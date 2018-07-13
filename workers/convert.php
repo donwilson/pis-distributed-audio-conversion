a46af07049221f2be9c46a54d93ed54ec4a416a8
Don Wilson <donwilson@gmail.com>
2018-07-13 12:18:40 -0500

    bug fix

--- workers/convert.php
+++ workers/convert.php
@@ -1,4 +1,8 @@
 <?php
 	// ./tools/ffmpeg-4.0.1-armel-32bit-static/ffmpeg -i "/san/pi_nas/source/101 Help Wanted.avi" -f mp4 -vcodec libx264 -preset fast -profile:v main -acodec aac "/san/pi_nas/converted/101 Help Wanted.mp4" -hide_banner
 	
+	require_once(__DIR__ ."/../config.php");
+	
+	use Pheanstalk\Pheanstalk;
+	
 	
